2bad3fd633227817f61c9748f7a7e8f6f3ef65be
ruba1995 <ruba_1995@outlook.com>
2020-12-16 22:01:23 +0200

    test.js

--- test.js
+++ test.js
@@ -3,11 +3,12 @@ alert("this websit is just for training ")
 var name = prompt("What is your name?");
  alert (" hi "+ name + " we want you to pick a number ");
  var number = prompt ("the number ?");
-  checknumber();
+  checknumber(number);
 function checknumber (number){
 
- if (number / 2==0  )
+ if ( number% 2==0  )
  { alert ("this number is even") ;}
- else { alert ("this numer is odd");}
+ else { alert ("this numer is odd");
+}
 }
  
